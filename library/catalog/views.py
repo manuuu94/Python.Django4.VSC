4ab69be447f3b7e00de668aad15cc112dacc2314
manu_ <manu_@LAPTOP-7HTT7HMN>
2022-12-27 19:28:13 -0600

    signup and sessions

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from .models import Book,Author,BookInstance,Genre,Language
 #class based view
 from django.views.generic import CreateView,DetailView
+#decorators (required to login to access X page) @login_required in the function
+from django.contrib.auth.decorators import login_required
+#mixins for class based views to require to authenticate first
+from django.contrib.auth.mixins import LoginRequiredMixin
+#model form for the userclass. dealing with the form to apply it into a classbasedview
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -21,10 +28,24 @@ def index(request):
     
     return render(request,'catalog/index.html',context=context)
 
-
-class BookCreate(CreateView): #model_form.html the default is to go to the BookDetail page (create view below)
+#classbasedview
+#mixin
+class BookCreate(LoginRequiredMixin,CreateView): #model_form.html the default is to go to the BookDetail page (create view below)
     model = Book
     fields = '__all__'
 
+#classbasedview
 class BookDetail(DetailView):
     model = Book
+
+#functionbasedview
+@login_required
+def my_view(request):
+    return render(request,'catalog/my_view.html')
+
+
+class SignUpView(CreateView):
+    #overwrites the form class with the usercreationform we imported
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'catalog/signup.html'
